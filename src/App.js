0cb6dc8e0d4740dbf9ac3d626777782c406cab67
Jesse Sandvik <sandvikjesse@gmail.com>
2022-04-05 22:26:21 -0400

    tested state changes with smoke tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import {useState} from 'react';
+
 const App = () => {
+  const [variable, setVariable] = useState('World');
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>{`Hello ${variable}!`}</h1>
     </div>
   );
 };
