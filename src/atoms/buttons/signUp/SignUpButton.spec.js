6bdb3cfcaa1a07ea5b05662b90ac8ecb9da66ddd
Jesse Sandvik <sandvikjesse@gmail.com>
2022-04-14 23:31:25 -0400

    refactored tests

--- src/atoms/buttons/signUp/SignUpButton.spec.js
+++ src/atoms/buttons/signUp/SignUpButton.spec.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import SignUpButton from './SignUpButton';
 
 describe('<SignUpButton />', () => {
   it('01. Renders the SignUp button component', () => {
-    render(<SignUpButton />);
+    render(<Router><SignUpButton /></Router>);
     expect(screen.getByRole('button', {name: 'sign up'})).toBeInTheDocument();
   });
 });
