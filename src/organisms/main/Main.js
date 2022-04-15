16df347d13621f90ee0290e1a834d9ecc0ec1622
Jesse Sandvik <sandvikjesse@gmail.com>
2022-04-14 20:35:20 -0400

    mapping routes from data file works for navigation and routes, tests passing

--- src/organisms/main/Main.js
+++ src/organisms/main/Main.js
@@ -1,14 +1,14 @@
 import {Routes, Route} from 'react-router-dom';
-import HomePage from '../../pages/home/HomePage';
 
 import {appRoutes} from '../../appData/appRoutes';
 
 const Main = () => {
+  const routesList = appRoutes.map((route, index) => (
+    <Route element={route.element} key={index} path={route.path} />
+  ));
   return (
     <main>
-      <Routes>
-        <Route path="/" element=<HomePage /> />
-      </Routes>
+      <Routes>{routesList}</Routes>
     </main>
   );
 };
