8b48c546def50f34de73a752fdd6baf2f47c827a
Jesse Sandvik <sandvikjesse@gmail.com>
2022-04-24 15:57:08 -0400

    updated first routing test for e2e

--- cypress/integration/routing/routing.spec.js
+++ cypress/integration/routing/routing.spec.js
@@ -1,6 +1,8 @@
 describe('Routing', () => {
   it('01. Displays the welcome message when navigating to the root', () => {
-    cy.visit('http://localhost:3000').contains('Welcome!');
+    cy.visit('http://localhost:3000');
+    cy.get('#app-title').click();
+    cy.contains('Welcome!');
   });
   it('02. Displays the register heading when user clicks the Sign Up button', () => {
     cy.visit('http://localhost:3000');
