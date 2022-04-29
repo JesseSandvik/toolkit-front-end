8cfcd3ce7573b207719b60b4aa4e1698b608c071
Jesse Sandvik <sandvikjesse@gmail.com>
2022-04-28 20:52:42 -0400

    added grid layout, background, border to main body

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,11 @@
 const HomePage = () => {
   return (
     <section>
-      <h2>Welcome!</h2>
+      <div className="container" id='top'>
+        <h2>Welcome!</h2>
+      </div>
+      <div className="container" id='mid'></div>
+      <div className="container" id='bot'></div>
     </section>
   );
 };
