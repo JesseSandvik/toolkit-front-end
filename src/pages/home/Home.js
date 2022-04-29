18409eaf083267435140d8a693974d94f757bb0b
Jesse Sandvik <sandvikjesse@gmail.com>
2022-04-28 21:12:41 -0400

    wireframed the main section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,11 @@
 const HomePage = () => {
   return (
     <section>
-      <div className="container" id='top'>
+      <div className="container" id='left'>
         <h2>Welcome!</h2>
       </div>
       <div className="container" id='mid'></div>
-      <div className="container" id='bot'></div>
+      <div className="container" id='right'></div>
     </section>
   );
 };
