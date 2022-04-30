35bab41ec9e6976f0d794275d8237c50ce9650a3
Jesse Sandvik <sandvikjesse@gmail.com>
2022-04-30 13:09:31 -0400

    homepage main section added heading and stylized, imported font

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,7 +4,9 @@ const HomePage = () => {
       <div className="container" id='left'>
         <h2>Welcome!</h2>
       </div>
-      <div className="container" id='mid'></div>
+      <div className="container" id='mid'>
+        <h2>Stacy's Cakes</h2>
+      </div>
       <div className="container" id='right'></div>
     </section>
   );
