216664a991fbc5ee99779b3ca9140b9cc421e335
Jesse Sandvik <sandvikjesse@gmail.com>
2022-04-29 21:23:43 -0400

    updated navigation tests for due to changes, added additional tests for each confirmed route

--- src/molecules/navigation/Navigation.spec.js
+++ src/molecules/navigation/Navigation.spec.js
@@ -3,11 +3,13 @@ import {render, screen} from '@testing-library/react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Navigation from './Navigiation';
 
+import {appRoutes} from '../../appData/appRoutes';
+
 describe('<Navigation />', () => {
   it('01. Renders the navigation', () => {
     render(
       <Router>
-        <Navigation />
+        <Navigation routes={appRoutes} />
       </Router>
     );
     expect(screen.getByRole('navigation')).toBeInTheDocument();
@@ -15,13 +17,26 @@ describe('<Navigation />', () => {
   it('02. Renders a list', () => {
     render(
       <Router>
-        <Navigation />
+        <Navigation routes={appRoutes} />
       </Router>
     );
     expect(screen.getByRole('list')).toBeInTheDocument();
   });
-  it('03. Renders the navigation link for home, "/"', () => {
-    render(<Router><Navigation /></Router>);
-    expect(screen.getByRole('link', {name: /home/i})).toBeInTheDocument();
+  it('03. Renders the navigation link for about, "/about"', () => {
+    render(<Router><Navigation routes={appRoutes} /></Router>);
+    expect(screen.queryByRole('link', {name: /savage/i})).not.toBeInTheDocument();
+    expect(screen.getByRole('link', {name: /about/i})).toBeInTheDocument();
+  });
+  it('04. Renders the navigation link for gallery, "/gallery"', () => {
+    render(<Router><Navigation routes={appRoutes} /></Router>);
+    expect(screen.getByRole('link', {name: /gallery/i})).toBeInTheDocument();
+  });
+  it('05. Renders the navigation link for menu, "/menu"', () => {
+    render(<Router><Navigation routes={appRoutes} /></Router>);
+    expect(screen.getByRole('link', {name: /menu/i})).toBeInTheDocument();
+  });
+  it('06. Renders the navigation link for contact, "/contact"', () => {
+    render(<Router><Navigation routes={appRoutes} /></Router>);
+    expect(screen.getByRole('link', {name: /contact/i})).toBeInTheDocument();
   });
 });
