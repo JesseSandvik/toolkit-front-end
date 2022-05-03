4e71eca6d9f7bdd76867cbc0912444eeaecb7296
Jesse Sandvik <sandvikjesse@gmail.com>
2022-05-03 08:12:58 -0400

    added image in bottom section of homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,3 +1,5 @@
+import iceCreamCake from '../../images/cake-custom-icecream.jpg';
+
 const HomePage = () => {
   return (
     <section>
@@ -6,6 +8,11 @@ const HomePage = () => {
           <p>Everything I create is custom made</p>
           <h3>with care</h3>
           <p>from my home for you!</p>
+        </div>
+        <div className="bottom">
+          <span className='image'>
+            <img src={iceCreamCake} alt='A colorful cake with an ice cream cone on top' />
+          </span>
         </div>
       </div>
       <div className="right"></div>
