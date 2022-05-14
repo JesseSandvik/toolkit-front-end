979ac68a76fb3d457972d5bd6e3407c9793dd232
Jesse Sandvik <sandvikjesse@gmail.com>
2022-05-13 23:15:55 -0400

    stripped out unnecessary css

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,18 +3,6 @@ import iceCreamCake from '../../images/cake-custom-icecream.jpg';
 const HomePage = () => {
   return (
     <section>
-      <div className="left">
-        <div className="top">
-          <p>Everything I create is custom made</p>
-          <h3>with care</h3>
-          <p>from my home for you!</p>
-        </div>
-        <div className="bottom">
-          <span className='image'>
-            <img src={iceCreamCake} alt='A colorful cake with an ice cream cone on top' />
-          </span>
-        </div>
-      </div>
     </section>
   );
 };
