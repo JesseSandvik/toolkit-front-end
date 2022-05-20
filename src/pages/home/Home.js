e10ea4620a658baa3ac5d3f48314bc3c5b8b80d5
Jesse Sandvik <sandvikjesse@gmail.com>
2022-05-19 21:35:57 -0400

    main body text for homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -7,7 +7,9 @@ const HomePage = () => {
     <section>
       <Paragraph>Everything from</Paragraph>
       <Heading level='2'>Stacy's Cakes</Heading>
-      <Paragraph>is custom made, with care, from my home for you!</Paragraph>
+      <Paragraph>is custom made</Paragraph>
+      <Heading level='2'>with care</Heading>
+      <Paragraph>fresh from my kitchen for you!</Paragraph>
     </section>
   );
 };
